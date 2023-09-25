dfc4833fef346c58c82c8c0353a8614db425e624
Juan Alegría <juanszalegria@gmail.com>
2023-09-25 00:55:29 -0500

    Fix: activate game objects for tv cameras on start

--- Assets/Scripts/TVs/ActivateAllDisplays.cs
+++ Assets/Scripts/TVs/ActivateAllDisplays.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class ActivateAllDisplays : MonoBehaviour
 {
+    [SerializeField] private GameObject[] componentsToActivate;
+
     void Start()
     {
+        foreach (GameObject component in componentsToActivate)
+        {
+            if (component != null)
+            {
+                component.SetActive(true);
+            }
+        }
+
         Debug.Log("displays connected: " + Display.displays.Length);
         // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
         // Check if additional displays are available and activate each.
@@ -13,10 +23,5 @@ public class ActivateAllDisplays : MonoBehaviour
         {
             Display.displays[i].Activate();
         }
-    }
-
-    void Update()
-    {
-
     }
 }
