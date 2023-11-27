ad2c94f10c8a992d2c3008b49779ed1cdc010769
AngelaJimenez <m.contrerasr@uniandes.edu.co>
2023-11-27 10:54:11 -0500

    Add buttons for change the floor

--- Assets/Scripts/MetaQuest2/TeleportButton.cs
+++ Assets/Scripts/MetaQuest2/TeleportButton.cs
@@ -1,15 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
 
-public class TeleportButton : MonoBehaviour
+public class TeleportButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
 {
+    public Color32 m_HoverColor = Color.blue;
+    public Color32 m_DownColor = Color.white;
+
+    private Image m_Image = null;
+    public GameObject objectToTeleport; // Asigna el objeto que quieres teletransportar en el Inspector
     public Vector3 destinationPosition = new Vector3(0.894f, -0.219f, 7.079f); // Establece la posición de destino
 
-    public void TeleportObject(GameObject objectToTeleport)
+    private void Awake()
     {
+        m_Image = GetComponent<Image>();
+    }
 
-        objectToTeleport.transform.position = destinationPosition;
+    private void TeleportObject()
+    {
+        if (objectToTeleport != null)
+        {
+            objectToTeleport.transform.position = destinationPosition;
+        }
+        else
+        {
+            Debug.LogError("No se ha asignado un objeto para teletransportar en el Inspector.");
+        }
+    }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("Click");
+        m_Image.color = m_HoverColor;
+        TeleportObject(); // Llama a la función de teletransporte cuando se hace clic
+    }
 
-
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("Down");
+        m_Image.color = m_DownColor;
     }
 }
