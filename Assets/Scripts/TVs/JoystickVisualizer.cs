a9bb1f6ff5bc39f93ea0a570f6e49bbfc83a90ce
Juan Alegría <juanszalegria@gmail.com>
2023-11-28 13:24:32 -0500

    Feat: improve joystick visualizer indicator

--- Assets/Scripts/TVs/JoystickVisualizer.cs
+++ Assets/Scripts/TVs/JoystickVisualizer.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Users;
 
 public class JoystickVisualizer : MonoBehaviour
 {
     public Image arrowImage;
-    public float rotationSpeed = 180f;
+    public Image rotationImage;
+    private PlayerInput playerInput;
+    private InputAction rotationAction;
+    public float rotationSpeed = 5f;
+
+    private void OnEnable()
+    {
+        playerInput = GetComponentInParent<PlayerInput>();
+        if (playerInput == null)
+        {
+            Debug.LogError("PlayerInput component not found. Please attach the PlayerInput component to this GameObject.");
+        }
+        rotationAction = playerInput.actions["Rotation"];
+    }
 
     void Update()
     {
+        // Arrow update
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
@@ -27,6 +45,33 @@ public class JoystickVisualizer : MonoBehaviour
         {
             // Disable the arrow image when there is no movement
             arrowImage.enabled = false;
+        }
+
+        // Rotation update
+        float Rx = rotationAction.ReadValue<Vector3>().x * rotationSpeed * Time.deltaTime;
+        float Ry = rotationAction.ReadValue<Vector3>().y * rotationSpeed * Time.deltaTime;
+        float Rz = rotationAction.ReadValue<Vector3>().z * rotationSpeed * Time.deltaTime;
+
+        // Check if there is joystick movement
+        if (Mathf.Abs(Rx) > 0.1f || Mathf.Abs(Ry) > 0.1f || Mathf.Abs(Rz) > 0.1f)
+        {
+            // Calculate the angle based on joystick input
+            float angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+
+            // Rotate the rotation image
+            if (rotationImage != null)
+            {
+                // Set the rotation of the arrow image
+                rotationImage.rectTransform.rotation = Quaternion.Euler(Rx, Ry, Rz);
+            }
+
+            // Enable the rotation image
+            rotationImage.enabled = true;
+        }
+        else
+        {
+            // Disable the arrow image when there is no movement
+            rotationImage.enabled = false;
         }
     }
 
